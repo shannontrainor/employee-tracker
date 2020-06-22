4ca7aec7dc8b12888e84531a51303c61ebac9ded
Shannon Trainor <sltrainor@outlook.com>
2020-06-21 20:45:26 -0400

    fixed add functions & update functions

--- db/index.js
+++ db/index.js
@@ -19,41 +19,26 @@ class DB {
         return this.connection.query("SELECT * FROM department");
     };
 
-    //add new employee
-    addNewEmployee(employee) {
-        return this.connection.query("INSERT INTO employee SET ?", employee);
-    }
+    //add new employee - first/last name, role, manager
+    addNewEmployee(first_name, last_name, role_id, manager_id) {
+        return this.connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?)", [first_name, last_name, role_id, manager_id]);
+    };
 
-    //add new role
-    addNewRole(role) {
-        return this.connection.query("INSERT INTO role SET ?", role);
-    }
+    //add new role - title, salar, dept id
+    addNewRole(title, salary, department_id) {
+        return this.connection.query("INSERT INTO role (title, salary, department_id) VALUES (?,?,?)", [title, salary, department_id]);
+    };
 
-    //add new dept
+    //add new dept - name
     addNewDepartment(department) {
         return this.connection.query("INSERT INTO department (name) VALUES (?)", [department]);
-    }
+    };
 
     //update role
-    updateRole(employeeId, roleId) {
-        return this.connection.query("UPDATE role SET role_id = ? WHERE id = ?", [roleId, employeeId ]);
-    }
+    updateRole(employee, role) {
+        return this.connection.query("UPDATE role SET role_id = ? WHERE id = ?", [employee, role]);
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+};
 
 module.exports = DB;
